83de75829f70383fc923e98aac29f67dc3da74c9
heewinkim <heewin.kim@gmail.com>
2020-03-23 18:56:13 +0900

    #200323 core __init__ import 안했던것 추가(PyData,Zip) deploy 1.0.8

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ require_packages=[
 packages = list(open('requirements.txt').readlines())
 setup(
     name='utilpack',
-    version='1.0.7',
+    version='1.0.8',
     author='HEESEUNG KIM',
     author_email='heewin.kim@gmail.com',
     description='Python Utils',
--- utilpack/core/__init__.py
+++ utilpack/core/__init__.py
@@ -4,5 +4,6 @@ from .maths import PyMaths
 from .logger import PyLogger
 from .output import PyOutput
 from .singleton import Singleton
+from .data import PyData,Zip
 from .error import PyError,ERROR_TYPES
 
