2ac8c34c8e3ee9f08d53863fc91ceda3a72f169d
heewinkim <heewin.kim@gmail.com>
2020-04-13 15:01:19 +0900

    #200413 deploy-1.2.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ require_packages=[
 packages = list(open('requirements.txt').readlines())
 setup(
     name='utilpack',
-    version='1.2.2',
+    version='1.2.3',
     author='HEESEUNG KIM',
     author_email='heewin.kim@gmail.com',
     description='Python Utils',
