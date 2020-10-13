ffdabb17e2f89d22e65529379fb77fa743d194eb
hian <hian@snaps.com>
2020-10-13 14:03:33 +0900

    #201012 hian 1.7.7 PyTime.get_timeFromFilename improve #2

--- utilpack/core/time.py
+++ utilpack/core/time.py
@@ -332,11 +332,10 @@ class PyTime(object):
         p3 = re.compile(r'\d{7,}')
         # 180501
         p4 = re.compile(r'[^0-9]{1}\d{2}[0-1][0-9][0-3][0-9][^0-9]{1}')
-        #
 
 
         :param filename: 파일 이름
-        :return: YYYYMMDD HH:MM:SS or None
+        :return: YYYYMMDD HH:MM:SS or ''
         """
 
         p = re.compile('[-: ]')
@@ -346,7 +345,7 @@ class PyTime(object):
         regexs = [
             r'\d{4}[0-1][0-9][0-3][0-9][^0-9]+[0-2][0-9][0-5][0-9][0-5][0-9]',
             r'\d{4}[0-1][0-9][0-3][0-9][0-2][0-9][0-5][0-9][0-5][0-9]',
-            r'\d{7,}',
+            r'\d{10,}',
             r'[^0-9]{1}\d{2}[0-1][0-9][0-3][0-9][^0-9]{1}'
         ]
         try:
@@ -359,15 +358,12 @@ class PyTime(object):
                 return str(datetime(int(timedata[:4]), int(timedata[4:6]), int(timedata[6:8]), int(timedata[8:10]),
                                     int(timedata[10:12]), int(timedata[12:14])))
             elif len(re.findall(regexs[2],filename_)) == 1:
-                timestamp = int(re.findall(regexs[2],filename_)[0])
-                if datetime(1971, 1, 1).timestamp() <= timestamp / 1000 <= datetime.now().timestamp():
-                    date = datetime.fromtimestamp(int(timestamp) / 1000)
-                    return datetime.strftime(date, '%Y-%m-%d %H:%M:%S')
-                elif datetime(1971, 1, 1).timestamp() <= timestamp <= datetime.now().timestamp():
+                timestamp = int(re.findall(regexs[2],filename_)[0][:10])
+                if datetime(1971, 1, 1).timestamp() <= timestamp <= datetime.now().timestamp():
                     date = datetime.fromtimestamp(int(timestamp))
                     return datetime.strftime(date, '%Y-%m-%d %H:%M:%S')
                 else:
-                    return None
+                    return ''
             elif len(re.findall(regexs[3],filename_)) == 1:
                 timedata = re.findall(regexs[3],filename_)[0][1:-1]
                 if int(filename_[1:3]) > 0:
@@ -376,7 +372,7 @@ class PyTime(object):
                     return str(datetime(int('19' + timedata[:2]), int(timedata[4:6]), int(timedata[6:8])))
 
         except Exception as e:
-            return None
+            return ''
 
 
 if __name__ == '__main__':
