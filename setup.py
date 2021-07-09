0cd4c748d64aebb8cb60a976e0b41ba075416a08
hian <hian@snaps.com>
2021-07-09 14:33:52 +0900

    #210708 hian update python version to 3.7.10, update requirements.txt

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ require_packages=[
 packages = list(open('requirements.txt').readlines())
 setup(
     name='utilpack',
-    version='1.9.3',
+    version='2.0.0',
     author='HEESEUNG KIM',
     author_email='heewin.kim@gmail.com',
     description='Python Utils',
