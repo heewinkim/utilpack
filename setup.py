f408a4e4d347a0d9cc0453dafb9c0b9e6227cd42
snaps-hian <hian@snaps.com>
2022-02-09 08:15:17 +0900

    #220209 hian algorithm.py docstring, data_util.py query2mysql_without_timeout added #2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ require_packages=[
 packages = list(open('requirements.txt').readlines())
 setup(
     name='utilpack',
-    version='2.0.7',
+    version='2.0.8',
     author='HEESEUNG KIM',
     author_email='heewin.kim@gmail.com',
     description='Python Utils',
