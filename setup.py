0f1aed18ad9934ca683ac9297cdc5ef9e1df75fd
snaps-hian <hian@snaps.com>
2022-06-03 16:50:53 +0900

    #220603 hian rm PyUI, update readme, pdf PyData to PyDataUtil #2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ require_packages=[
 packages = list(open('requirements.txt').readlines())
 setup(
     name='utilpack',
-    version='2.1.9',
+    version='2.2.0',
     author='HEESEUNG KIM',
     author_email='heewin.kim@gmail.com',
     description='Python Utils',
