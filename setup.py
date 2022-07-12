8f256d3a41e947edee8e94c16d8f12b6ab8c472a
snaps-hian <hian@snaps.com>
2022-07-12 11:59:02 +0900

    #220712 hian Pillow to Pillow-SIMD #2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ require_packages=[
 packages = list(open('requirements.txt').readlines())
 setup(
     name='utilpack',
-    version='2.2.0',
+    version='2.2.1',
     author='HEESEUNG KIM',
     author_email='heewin.kim@gmail.com',
     description='Python Utils',
