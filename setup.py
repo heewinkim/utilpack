91393b94b40349bb94598dbc23f7612782153a55
snaps-hian <hian@snaps.com>
2022-09-23 18:22:03 +0900

    #220923 hian Pillow-SIMD to Pillow

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ require_packages=[
     'matplotlib',
     'sklearn',
     'pandas',
-    'Pillow-SIMD',
+    'Pillow',
     'opencv-python',
     'pymysql',
     'pycryptodome',
@@ -23,7 +23,7 @@ require_packages=[
 packages = list(open('requirements.txt').readlines())
 setup(
     name='utilpack',
-    version='2.2.1',
+    version='2.2.2',
     author='HEESEUNG KIM',
     author_email='heewin.kim@gmail.com',
     description='Python Utils',
