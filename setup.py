ddb8941a88582d4db7c07653699872f2f0f29d94
heewinkim <heewin.kim@gmail.com>
2023-03-13 13:39:21 +0900

    Update setup.py
    version up to 2.2.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ require_packages=[
 packages = list(open('requirements.txt').readlines())
 setup(
     name='utilpack',
-    version='2.2.3',
+    version='2.2.5',
     author='HEESEUNG KIM',
     author_email='heewin.kim@gmail.com',
     description='Python Utils',
