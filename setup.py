a8c98d2848ed02e07480831003b32b74473fa238
hian <hian@snaps.com>
2025-04-22 13:34:25 +0900

    version up

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ require_packages=[
 packages = list(open('requirements.txt').readlines())
 setup(
     name='utilpack',
-    version='2.3.1',
+    version='2.3.2',
     author='HEESEUNG KIM',
     author_email='heewin.kim@gmail.com',
     description='Python Utils',
